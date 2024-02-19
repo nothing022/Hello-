26256772441981189922fe4abb3a6504658990ff
Tirox-XF <160381986+cxiox@users.noreply.github.com>
2024-02-19 15:38:48 +0530

    Update suggestion.py

--- AnnieRobot/plugins/misc/suggestion.py
+++ AnnieRobot/plugins/misc/suggestion.py
@@ -1,17 +1,6 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 import asyncio
 import random
 from datetime import datetime, timedelta
-
 import config
 from config import clean
 from strings import get_string
