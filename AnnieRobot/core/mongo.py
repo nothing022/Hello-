b0920f990e0c7d475824a5493ddf0f05cee256c3
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-19 14:15:10 +0530

    Update and rename mongo (2).py to mongo.py

--- AnnieRobot/core/mongo.py
+++ AnnieRobot/core/mongo.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
@@ -40,5 +31,5 @@ if config.MONGO_DB_URI is None:
 else:
     _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
     _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
-    mongodb = _mongo_async_.Yukki
-    pymongodb = _mongo_sync_.Yukki
+    mongodb = _mongo_async_.Annie
+    pymongodb = _mongo_sync_.Annie
