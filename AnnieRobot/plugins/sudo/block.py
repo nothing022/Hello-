6131d7964d14d04a07c5625fea09069f6b709aaf
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-19 15:47:43 +0530

    Update block.py

--- AnnieRobot/plugins/sudo/block.py
+++ AnnieRobot/plugins/sudo/block.py
@@ -13,10 +13,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_gban_user, remove_gban_user
-from YukkiMusic.utils.decorators.language import language
+from AnnieRobot import app
+from AnnieRobot.misc import SUDOERS
+from AnnieRobot.utils.database import add_gban_user, remove_gban_user
+from AnnieRobot.utils.decorators.language import language
 
 # Command
 BLOCK_COMMAND = get_command("BLOCK_COMMAND")
