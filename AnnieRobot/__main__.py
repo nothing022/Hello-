40d72ec90c99cc51e083dfff0b9e680787645a2f
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-19 13:51:36 +0530

    Create __main__.py

---
+++ AnnieRobot/__main__.py
@@ -0,0 +1,73 @@
+
+import asyncio
+import importlib
+import sys
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from config import BANNED_USERS
+from AnnieRobit import LOGGER, app, userbot
+from AnnieRobot.core.call import Yukki
+from AnnieRobot.plugins import ALL_MODULES
+from AnnieRobot.utils.database import get_banned_users, get_gbanned
+
+loop = asyncio.get_event_loop_policy().get_event_loop()
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("AnnieRobot").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if (
+        not config.SPOTIFY_CLIENT_ID
+        and not config.SPOTIFY_CLIENT_SECRET
+    ):
+        LOGGER("AnnieRobot").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("Annierobot.plugins" + all_module)
+    LOGGER("AnnieRobot.plugins").info(
+        "Successfully Imported Modules "
+    )
+    await userbot.start()
+    await Annie.start()
+    try:
+        await Annie.stream_call(
+            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
+        )
+    except NoActiveGroupCall:
+        LOGGER("AnnieRobot").error(
+            "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group"
+        )
+        sys.exit()
+    except:
+        pass
+    await Annie.decorators()
+    LOGGER("AnnieRobot").info(" Bot Started Successfully")
+    await idle()
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(init())
+    LOGGER("AnnieRobot").info("Stopping  Music Bot! GoodBye")
