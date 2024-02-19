0a16e75eda83a6c06b413eab80324d83c650f7f5
Tirox-XF <160381986+cxiox@users.noreply.github.com>
2024-02-19 14:21:04 +0530

    Update bot.py

--- AnnieRobot/core/bot.py
+++ AnnieRobot/core/bot.py
@@ -13,7 +13,7 @@ class AnnieBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "YukkiMusicBot",
+            "AnnieRobot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
