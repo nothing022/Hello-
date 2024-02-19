d77f85f26f7783048a67ab2b5e9bc9bf48941416
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-19 14:50:45 +0530

    Update loop.py

--- AnnieRobot/plugins/admins/loop.py
+++ AnnieRobot/plugins/admins/loop.py
@@ -13,10 +13,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.utils.database.memorydatabase import (get_loop,
+from AnnieRobot import app
+from AnnieRobot.utils.database.memorydatabase import (get_loop,
                                                       set_loop)
-from YukkiMusic.utils.decorators import AdminRightsCheck
+from AnnieRobot.utils.decorators import AdminRightsCheck
 
 # Commands
 LOOP_COMMAND = get_command("LOOP_COMMAND")
