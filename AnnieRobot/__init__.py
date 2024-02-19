44d1138b3eaeabdf294d194e907457288045244d
Tirox-XF <160381986+cxiox@users.noreply.github.com>
2024-02-19 17:39:01 +0530

    Update __init__.py

--- AnnieRobot/__init__.py
+++ AnnieRobot/__init__.py
@@ -3,18 +3,8 @@ from AnnieRobot.core.dir import dirr
 from AnnieRobot.core.git import git
 from AnnieRobot.core.userbot import Userbot
 from AnnieRobot.misc import dbb, heroku, sudo
-from AnnieRobot.platforms import *
+
 from .logging import LOGGER
-import asyncio
-import os
-import re
-from typing import Union
-from pyrogram.enums import MessageEntityType
-from pyrogram.types import Message
-from youtubesearchpython.__future__ import VideosSearch
-
-
-
 
 # Directories
 dirr()
@@ -37,13 +27,12 @@ app = AnnieBot()
 # Assistant Client
 userbot = Userbot()
 
-
 from .platforms import *
 
-Apple = AppleAPI()
+YouTube = YouTubeAPI()
 Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
 Resso = RessoAPI()
 SoundCloud = SoundAPI()
-Spotify = SpotifyAPI()
 Telegram = TeleAPI()
-YouTube = YouTubeAPI()
