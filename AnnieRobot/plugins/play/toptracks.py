8c018b3b0891830c54eed8bfba15c5f750e97036
Tirox-XF <160381986+cxiox@users.noreply.github.com>
2024-02-19 15:47:01 +0530

    Update toptracks.py

--- AnnieRobot/plugins/play/toptracks.py
+++ AnnieRobot/plugins/play/toptracks.py
@@ -1,18 +1,6 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 import asyncio
-
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup
-
 from config import BANNED_USERS
 from AnnieRobot import app
 from AnnieRobot.utils.database import (get_global_tops,
