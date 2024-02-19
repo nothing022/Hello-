f0af2ada2a0a1628c96a4994cfb225dd09f59a10
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-19 15:50:43 +0530

    Update logger.py

--- AnnieRobot/plugins/sudo/logger.py
+++ AnnieRobot/plugins/sudo/logger.py
@@ -12,10 +12,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database import add_off, add_on
-from YukkiMusic.utils.decorators.language import language
+from AnnieRobot import app
+from AnnieRobot.misc import SUDOERS
+from AnnieRobot.utils.database import add_off, add_on
+from AnnieRobot.utils.decorators.language import language
 
 # Commands
 LOGGER_COMMAND = get_command("LOGGER_COMMAND")
