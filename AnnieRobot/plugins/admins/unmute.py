67efbd96708aba8b54ff9f0d65fbb7bddddfa88f
Tirox-XF <160381986+cxiox@users.noreply.github.com>
2024-02-19 15:34:52 +0530

    Update unmute.py

--- AnnieRobot/plugins/admins/unmute.py
+++ AnnieRobot/plugins/admins/unmute.py
@@ -1,16 +1,5 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#s
-
 from pyrogram import filters
 from pyrogram.types import Message
-
 from config import BANNED_USERS
 from strings import get_command
 from AnnieRobot import app
@@ -34,7 +23,7 @@ async def unmute_admin(Client, message: Message, _, chat_id):
     if not await is_muted(chat_id):
         return await message.reply_text(_["admin_7"])
     await mute_off(chat_id)
-    await Yukki.unmute_stream(chat_id)
+    await Annie.unmute_stream(chat_id)
     await message.reply_text(
         _["admin_8"].format(message.from_user.mention)
     )
