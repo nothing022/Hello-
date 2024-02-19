91135ecac818a96feacebd28f6917ae17e06f2ca
Tirox-XF <160381986+cxiox@users.noreply.github.com>
2024-02-19 17:06:22 +0530

    Update Spotify.py

--- AnnieRobot/platforms/Spotify.py
+++ AnnieRobot/platforms/Spotify.py
@@ -1,19 +1,7 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 import re
-
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from youtubesearchpython.__future__ import VideosSearch
-
 import config
 
 
