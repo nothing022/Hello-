438594e8aca0d881b4d446b1a1e7dd0a03a6b69e
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-19 15:41:36 +0530

    Update toptracks.py
    Yeahhhhhhhhhhh

--- AnnieRobot/plugins/play/toptracks.py
+++ AnnieRobot/plugins/play/toptracks.py
@@ -14,14 +14,14 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup
 
 from config import BANNED_USERS
-from YukkiMusic import app
-from YukkiMusic.utils.database import (get_global_tops,
+from AnnieRobot import app
+from AnnieRobot.utils.database import (get_global_tops,
                                        get_particulars, get_userss)
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.inline.playlist import (botplaylist_markup,
+from AnnieRobot.utils.decorators.language import languageCB
+from AnnieRobot.utils.inline.playlist import (botplaylist_markup,
                                               failed_top_markup,
                                               top_play_markup)
-from YukkiMusic.utils.stream.stream import stream
+from AnnieRobot.utils.stream.stream import stream
 
 loop = asyncio.get_running_loop()
 
