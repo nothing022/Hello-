66de1beaff3ba61b791d9b496721f2128f2dfc65
Tirox-XF <160381986+cxiox@users.noreply.github.com>
2024-02-19 14:50:10 +0530

    Update __init__.py

--- AnnieRobot/__init__.py
+++ AnnieRobot/__init__.py
@@ -1,1 +1,38 @@
+from AnnieRobot.core.bot import AnnieBot
+from AnnieRobot.core.dir import dirr
+from AnnieRobot.core.git import git
+from AnnieRobot.core.userbot import Userbot
+from AnnieRobot.misc import dbb, heroku, sudo
 
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = AnnieBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
