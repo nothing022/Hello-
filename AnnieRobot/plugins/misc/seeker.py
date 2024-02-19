ad4cbb7921659d71b5d2d165cbe9d667728bf177
Tirox-XF <160381986+cxiox@users.noreply.github.com>
2024-02-19 15:38:28 +0530

    Update seeker.py

--- AnnieRobot/plugins/misc/seeker.py
+++ AnnieRobot/plugins/misc/seeker.py
@@ -1,8 +1,5 @@
-
 import asyncio
-
 from pyrogram.types import InlineKeyboardMarkup
-
 from strings import get_string
 from AnnieRobot.misc import db
 from AnnieRobot.utils.database import (get_active_chats, get_lang,
