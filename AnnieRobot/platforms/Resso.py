f495ddcc0c21b2c14c4b886700d3d63acc2f2090
Tirox-XF <160381986+cxiox@users.noreply.github.com>
2024-02-19 16:48:45 +0530

    Update Resso.py

--- AnnieRobot/platforms/Resso.py
+++ AnnieRobot/platforms/Resso.py
@@ -1,16 +1,5 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 import re
 from typing import Union
-
 import aiohttp
 from bs4 import BeautifulSoup
 from youtubesearchpython.__future__ import VideosSearch
