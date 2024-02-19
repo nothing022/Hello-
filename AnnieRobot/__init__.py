d7f002534f1b8c722a6a74e871effd282a1a55ff
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-19 16:53:40 +0530

    Update __init__.py

--- AnnieRobot/__init__.py
+++ AnnieRobot/__init__.py
@@ -5,6 +5,19 @@ from AnnieRobot.core.userbot import Userbot
 from AnnieRobot.misc import dbb, heroku, sudo
 from AnnieRobot.platforms import *
 from .logging import LOGGER
+import asyncio
+import os
+import re
+from typing import Union
+
+from pyrogram.enums import MessageEntityType
+from pyrogram.types import Message
+from yt_dlp import YoutubeDL
+from youtubesearchpython.__future__ import VideosSearch
+
+
+
+
 
 # Directories
 dirr()
@@ -26,6 +39,19 @@ app = AnnieBot()
 
 # Assistant Client
 userbot = Userbot()
+
+
+
+class YouTubeAPI:
+    def __init__(self):
+        self.base = "https://www.youtube.com/watch?v="
+        self.regex = r"(?:youtube\.com|youtu\.be)"
+        self.status = "https://www.youtube.com/oembed?url="
+        self.listbase = "https://youtube.com/playlist?list="
+        self.reg = re.compile(
+            r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])"
+        )
+      
 
 from .platforms import *
 
