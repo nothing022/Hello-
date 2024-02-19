5aff2a40b361abe890f132aaa39db4b04f825067
unknownteam72 <148250637+unknownteam72@users.noreply.github.com>
2024-02-19 14:28:16 +0530

    Update Soundcloud.py

--- AnnieRobot/platforms/Soundcloud.py
+++ AnnieRobot/platforms/Soundcloud.py
@@ -13,7 +13,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from YukkiMusic.utils.formatters import seconds_to_min
+from AnnieRobot.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
